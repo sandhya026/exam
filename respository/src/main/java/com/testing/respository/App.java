0753119adbd9f234497e0214aba3ff753755244b
91938 <91938@sowmya1234>
2022-06-18 12:52:02 +0530

    final exam commit

---
+++ respository/src/main/java/com/testing/respository/App.java
@@ -0,0 +1,13 @@
+package com.testing.respository;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
